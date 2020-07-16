091daa1fd18932f027abbf2373cd0fc4ae7fe173
Yoshino-s <yoshino.prog@gmail.com>
2020-07-16 13:51:30 +0800

    fix(repeater): fix repeater on image

--- bot/src/plugins/repeater.ts
+++ bot/src/plugins/repeater.ts
@@ -1,12 +1,26 @@
 import { MiraiBot } from "../bot/Bot";
 import { serialize } from "../bot/serialization";
+import { diffChars } from "diff";
+
+function similarity(s0: string, s1: string) {
+  return (
+    (diffChars(s0, s1).reduce(
+      (p, c) => (!(c.added || c.removed) ? p + c.value.length : p),
+      0
+    ) /
+      (s0.length + s1.length)) *
+    2
+  );
+}
 export default function RepeaterPlugin(bot: MiraiBot) {
   const msgSet = new Map<number, [string, number]>();
   bot.mirai.on("message", (msg) => {
     if (msg.type === "FriendMessage") return;
     const plain = serialize(msg.messageChain);
     const p = msgSet.get(msg.sender.group.id) ?? [plain, 0];
-    if (p[0] === plain) {
+    const s = similarity(plain, p[0]);
+    console.log(s, plain);
+    if (s > 0.6) {
       p[1]++;
     } else {
       p[0] = plain;
