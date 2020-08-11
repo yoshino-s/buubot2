b0d2a33457fa1872792b1d42cb41bfaba8199e54
Yoshino-s <yoshino.prog@gmail.com>
2020-08-11 20:34:51 +0800

    fix(search): move proxy to cloudflare

--- bot/src/plugins/search.ts
+++ bot/src/plugins/search.ts
@@ -1,22 +1,22 @@
 import { MiraiBot } from "../bot/Bot";
 import { agent } from "../bot/utils/proxy";
+import Axios from "axios";
 export default function SearchPlugin(bot: MiraiBot) {
   bot.registerCommand(
     {
       cmd: "search",
     },
     (msg, cmd, args) =>
-      agent
-        .get(
-          `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(
-            args
-          )}`,
-          {
-            headers: {
-              "accept-language": "zh-CN,zh;q=0.9",
-            },
-          }
-        )
+      Axios.get(
+        `https://wiki.yoshino-s.workers.dev/api/rest_v1/page/summary/${encodeURIComponent(
+          args
+        )}`,
+        {
+          headers: {
+            "accept-language": "zh-CN,zh;q=0.9",
+          },
+        }
+      )
         .then((i) => i.data.extract)
         .catch(() => "找不到呢")
   );
