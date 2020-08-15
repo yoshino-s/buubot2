f49adaeb936edec1c6077c5cb51f4a04d2479f3b
Yoshino-s <yoshino.prog@gmail.com>
2020-08-15 21:53:54 +0800

    fix(ban): add recall msg

--- bot/src/plugins/banner.ts
+++ bot/src/plugins/banner.ts
@@ -7,7 +7,7 @@ export default function BannerPlugin(bot: MiraiBot) {
   bot.registerCommand(
     {
       cmd: "ban",
-      help: "Usage: rank word",
+      help: "Usage: ban word",
       rule: CommandPermission.admin,
       verify: (msg, cmd, args) => !!args,
     },
@@ -19,11 +19,27 @@ export default function BannerPlugin(bot: MiraiBot) {
       return `将对关键词"${args}"封禁。现在的封禁词列表：${w.join(",")}`;
     }
   );
+  bot.registerCommand(
+    {
+      cmd: "unban",
+      help: "Usage: unban word",
+      rule: CommandPermission.admin,
+      verify: (msg, cmd, args) => !!args,
+    },
+    async (msg, cmd, args) => {
+      const target = extractTarget(msg, false);
+      let w = (await banWorkMap.get(target)) || [];
+      w = w.filter((v) => v !== args.toLowerCase());
+      await banWorkMap.set(target, w);
+      return `将对关键词"${args}"解封。现在的封禁词列表：${w.join(",")}`;
+    }
+  );
   bot.mirai.on("GroupMessage", async (msg) => {
     const target = extractTarget(msg, false);
     const w = (await banWorkMap.get(target)) || [];
     const plain = msg.plain.toLowerCase();
     if (w.some((v) => plain.includes(v))) {
+      await bot.mirai.api.recall(msg);
       await bot.mirai.api.mute(msg.sender.group.id, msg.sender.id, 60 * 60);
       await bot.send(
         target,
